0e3b10a3a92b15d5711eb9faad4b89dc6a0d5768
masaru <massaru129@gmail.com>
2014-06-23 17:39:54 +0900

    subjectモデルの作成、db設定を行った

--- shiketai_tool/db/migrate/20140623064229_create_subjects.rb
+++ shiketai_tool/db/migrate/20140623064229_create_subjects.rb
@@ -3,7 +3,10 @@ class CreateSubjects < ActiveRecord::Migration
     create_table :subjects do |t|
 	  t.string :name, null:false
 	  t.string :teacher_id, null:false
-	  t.string :description
+	  t.integer :day_of_a_week_id, null:false
+	  t.integer :period_id, null:false
+	  t.integer :semester_id, null:false
+	  t.string :description, null:false
 				   
 
       t.timestamps
