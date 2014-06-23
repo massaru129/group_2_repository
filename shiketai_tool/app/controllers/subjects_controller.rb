1dc7163dc707b44bc665b9d2430c4a6310e06b10
masaru <massaru129@gmail.com>
2014-06-23 18:42:24 +0900

    subjectコントローラーのメソッドを追加、修正 #1

--- shiketai_tool/app/controllers/subjects_controller.rb
+++ shiketai_tool/app/controllers/subjects_controller.rb
@@ -9,13 +9,32 @@ class SubjectsController < ApplicationController
 	  redirect_to @subject
 	end
   end
-
+  
   def show
     @subject = Subject.find(params[:id])
   end
 
   def index
     @subjects = Subject.all
+  end
+
+  def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    @subject = Subject.find(params[:id])
+	if @subject.update(subject_params)
+	  redirect_to @subject
+	else
+	  render 'edit'
+	end
+  end
+  
+  def destroy
+    @subject = Subject.find(params[:id])
+	@subject.destroy
+	redirect_to subujects_path
   end
 
   private
