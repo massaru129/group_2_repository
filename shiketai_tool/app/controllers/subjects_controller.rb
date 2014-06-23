1a88dd20f449d6b32b20ea21be730a231dfd9009
masaru <massaru129@gmail.com>
2014-06-23 15:50:52 +0900

    subjectコントローラーにメソッドを追加 #1

--- shiketai_tool/app/controllers/subjects_controller.rb
+++ shiketai_tool/app/controllers/subjects_controller.rb
@@ -1,2 +1,20 @@
 class SubjectsController < ApplicationController
+  def new
+    @subject = Subejct.new
+  end
+
+  def create
+    @subject = Subject.new(subject_params)
+	if @subject.save
+	  redirect_to @subject
+	end
+  end
+
+  def show
+    @subject = Subject.find(param[:id])
+  end
+
+  def index
+    @projects = Project.all
+  end
 end
