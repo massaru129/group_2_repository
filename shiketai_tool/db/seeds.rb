61633c20fa7e0518e3e08bed8a1dd560ba2235c8
masaru <massaru129@gmail.com>
2014-06-28 19:31:49 +0900

    曜日データを挿入するスクリプトを作成 #6

--- shiketai_tool/db/seeds.rb
+++ shiketai_tool/db/seeds.rb
@@ -8,10 +8,19 @@
 
 # 時限情報を挿入
 require 'date'
-Period.create(period_name: '1限', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
-Period.create(period_name: '2限', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
-Period.create(period_name: '3限', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
-Period.create(period_name: '4限', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
-Period.create(period_name: '5限', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
-Period.create(period_name: '6限', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
-Period.create(period_name: 'その他', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+Period.create(period: '1限', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+Period.create(period: '2限', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+Period.create(period: '3限', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+Period.create(period: '4限', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+Period.create(period: '5限', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+Period.create(period: '6限', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+Period.create(period: 'その他', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+
+# 曜日情報を挿入
+DayOfAWeek.create(day_of_a_week: '月曜日', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+DayOfAWeek.create(day_of_a_week: '火曜日', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+DayOfAWeek.create(day_of_a_week: '水曜日', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+DayOfAWeek.create(day_of_a_week: '木曜日', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+DayOfAWeek.create(day_of_a_week: '金曜日', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+DayOfAWeek.create(day_of_a_week: '土曜日', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
+DayOfAWeek.create(day_of_a_week: '集中講義', created_at: DateTime.now.to_s(:db), updated_at:DateTime.now.to_s(:db))
