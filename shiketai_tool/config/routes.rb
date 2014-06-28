165190fd20fdf722dc66a500eac71a996c2f1ae6
masaru <massaru129@gmail.com>
2014-06-28 20:41:44 +0900

    rootを変更

--- shiketai_tool/config/routes.rb
+++ shiketai_tool/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root 'welcome#index'
   
   resources :subjects
   # The priority is based upon order of creation: first created -> highest priority.
