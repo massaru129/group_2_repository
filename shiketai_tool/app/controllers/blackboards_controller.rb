f4f7097610719b905a05cbb52cb86227972dc844
masaru <massaru129@gmail.com>
2014-06-30 15:13:29 +0900

    controllerをコピペ

--- shiketai_tool/app/controllers/blackboards_controller.rb
+++ shiketai_tool/app/controllers/blackboards_controller.rb
@@ -1,13 +1,46 @@
 class BlackboardsController < ApplicationController
   def new
+    @blackboard = Blackboard.new
   end
 
   def create
+    @blackboard = Blackboard.new(blackboard_params)	
+    if @blackboard.save
+      redirect_to @blackboard
+	else
+	  render 'new'
+	end
+  end
+
+  def index
+    @blackboards = Blackboard.all
   end
 
   def show
+    @blackboard = Blackboard.find(params[:id])
   end
 
   def edit
+    @blackboard = Blackboard.find(params[:id])
   end
+
+  def update
+    @blackboard = Blackboard.find(params[:id])
+    if @blackboard.update(blackboard_params)
+      redirect_to @blackboard
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @blackboard = Blackboard.find(params[:id])
+	@blackboard.destroy
+	redirect_to blackboards_path
+  end
+
+  private
+    def blackboard_params
+	  params.require(:blackboard).permit(:title, :description, :material)
+	end
 end
